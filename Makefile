56e03ec001aae3581673aa257dcb03e91a325b9e
intern_dzs <vuhailongkl97@gmail.com>
2019-08-22 11:19:15 +0900

    hihi

---
+++ Makefile
@@ -0,0 +1,8 @@
+override FLAGS +=
+FILE_IN=main.c
+all:
+	gcc -o t $(FLAGS) $(FILE_IN)
+
+clean_trash:
+	rm -f output2.txt 
+
---
+++ main.c
@@ -0,0 +1,29 @@
+#include"stdlib.h"
+#include"math.h"
+#include"unistd.h"
+#include"lib.h"
+#include"pthread.h"
+#include"stdio.h"
+
+
+void *func(void *vargp)
+{
+        sleep(1);
+        printf("Printing GeeksQuiz from Thread \n");
+	    printf("sqrt(16) is %lf", sqrt(16));
+		return NULL;
+}
+
+int main()
+{
+		pthread_t t;
+
+		kkk();
+		pthread_create(&t, NULL, func, NULL);
+		pthread_join(t, NULL);
+
+		pthread_exit(NULL);
+		puts("hello");
+        exit(0);
+}
+
